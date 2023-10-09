059feddc60b255698f687abf31aef19ed1ed25f9
joao200920 <113525545+joao200920@users.noreply.github.com>
2023-10-09 08:09:30 -0300

    Add files via upload

--- Peso.js
+++ Peso.js
@@ -1,26 +1,24 @@
-//Cria referência ao form e elementos de resposta do programa
-
 const form = document.querySelector("form")
-const resp1 = document.querySelector("h3")
-const resp2 = document.querySelector("h4")
+const resp = document.querySelector("h3")
 
-//Cria um ouvinte
-form.addEventListener("submit",(e)=>{e.preventDefault()   //evita o envio do form
- const nome = form.inNome.value            //obtém o nome
- const nota1 = Number(form,inNota1.value)  //obtém a primeira nota
- const nota2 = Number(form.inNota2.value)  //obtém a segunda nota
- const media = (nota1 + nota2.value)       //média das notas
- resp1.innerText = `Média das Notas ${media.toFixed(2)}`
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
 
- //Cria as condicionais
- if(media >= 7) {
-    resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)`
-    resp2.style.color = "blue"
- } else if (media >=4) {
-    resp2.innerText = `Atenção ${nome}! Você está de exame`
-    resp2.style.color = "green"
- } else{
-    resp2.innerText = `Ops ${nome}! Você foi Reprovado(a)`
-    resp2.style.color = "red"
- }
+    const nome = form.inNome.value
+    const masculino = form.inMasculino.checked
+    const altura = Number(form.inAltura.value)
+
+    let peso
+    if (masculino) {
+        peso = 22 * Math.pow(altura, 2)
+    } else {
+        peso = 21 * Math.pow(altura, 2)
+        
+    }
+
+    resp.innerText = `${nome} seu peso ideal é: ${peso.toFixed(3)}kg`
+})
+
+form.addEventListener("reset", (e) => {
+    resp.innerText = " "
 })
